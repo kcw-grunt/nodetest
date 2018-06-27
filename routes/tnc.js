be448efd7c03267432fa190e6a8771dcf4eb7ee7
Kerry <mrkerrywashington@icloud.com>
2018-06-27 01:22:42 -0700

    trying more

--- routes/tnc.js
+++ routes/tnc.js
@@ -15,13 +15,33 @@ console.log('TNC set...');
 
 function send_string(str) {
 
-    const packet = new ax25.Packet();
-    packet.type = ax25.Defs.U_FRAME_UI;
-    packet.sourceCallsign = 'KM6TIG';
-    packet.sourceSSID = 1;
-    packet.destinationCallsign = 'KM6TIG';
-    packet.destinationSSID = 2;
-    packet.infoString == 'HELLO Stupid test string';
+
+    var packet = new ax25.Packet(
+        {	'destinationCallsign'	: "KM6TIG",
+            'destinationSSID'		: 1,
+            'sourceCallsign'		: "KM6TIGE",
+            'sourceSSID'			: 2,
+            'pollFinal'				: true,
+            'command'				: true,
+            'type'					: ax25.Defs.I_FRAME,
+            'nr'					: 1,
+            'ns'					: 3,
+            'pid'					: ax25.Defs.PID_NONE,
+            'infoString'			: "Your mother."
+        }
+    );
+
+
+
+
+
+
+    // packet.type = ax25.Defs.U_FRAME_UI;
+    // packet.sourceCallsign = 'KM6TIG';
+    // packet.sourceSSID = 1;
+    // packet.destinationCallsign = 'KM6TIG';
+    // packet.destinationSSID = 2;
+    // packet.infoString == 'HELLO Stupid test string';
     tnc.send(packet.assemble(), () => console.log('Sent:', str));
 }
 
@@ -31,6 +51,20 @@ router.get('/', function (req,res) {
     //res.send('GET handler for the /tnc route');
     messageString = 'Send test packet';
     send_string(messageString); 
+
+    tnc.on(
+        "frame",
+        function(frame) {
+            console.log("Here's an array of bytes representing an AX.25 frame: " + frame);
+        }
+    );
+    
+    tnc.on(
+        "error",
+        function(err) {
+            console.log("HURRRRR! I DONE BORKED!" + err);
+        }
+    );
     res.send({ response: 'Sent:'+ messageString }); 
 });
 
