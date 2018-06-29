272939591ed1c906a4c8a6539c0775cfbdad097f
Kerry <mrkerrywashington@icloud.com>
2018-06-29 08:09:13 -0700

    update npm packages

--- routes/tnc.js
+++ routes/tnc.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var ax25 = require('th-d72-ax25');
+var SerialPort = require('serialport');
 var messageString = '';
 console.log('Turn Kenwood THD72A on and set to Packet 12 \nPressing TNC');
 
-
-
-var SerialPort = require('serialport');
 var devicePath = '/dev/ttyUSB0'; 
 const parsers = SerialPort.parsers;
 // Use a `\r\n` as a line terminator
@@ -14,13 +12,19 @@ const parser = new parsers.Readline({
     delimiter: '\r\n'
   }); 
 
-// Set port path regardless of OS
-ax25.kissTNC.serialPort.list(function (err, ports) {
-    ports.forEach(function(port) {
-      if (port.comName === '/dev/tty.SLAB_USBtoUART' || port.comName === '/dev/ttyUSB0') {
-        devicePath = port.comName;
-      }
-});
+//Set port path regardless of OS
+
+// const sp = SerialPort(devicePath,9600);
+
+
+// sp.list(function (err, ports) {
+//     ports.forEach(function(port) {
+//       if (port.comName === '/dev/tty.SLAB_USBtoUART' || port.comName === '/dev/ttyUSB0') {
+//         devicePath = port.comName;
+//       }
+//     });
+// });
+ 
 
 console.log('Selected port: '+ devicePath +'\n');
 
@@ -35,58 +39,13 @@ var tnc = new ax25.kissTNC(
 ///dev/ttyUSB0
 console.log('TNC set...');
 
-function send_string(str) {
-
-
-    var packet = new ax25.Packet(
-        {	'destinationCallsign'	: "KM6TIG",
-            'destinationSSID'		: 1,
-            'sourceCallsign'		: "KM6TIGE",
-            'sourceSSID'			: 2,
-            'pollFinal'				: true,
-            'command'				: true,
-            'type'					: ax25.Defs.U_FRAME_UI,
-            'nr'					: 1,
-            'ns'					: 3,
-            'pid'					: ax25.Defs.PID_NONE,
-            'infoString'			: "Your mother."
-        }
-    );
-    console.log(packet);
- 
-
-    // packet.type = ax25.Defs.U_FRAME_UI;
-    // packet.sourceCallsign = 'KM6TIG';
-    // packet.sourceSSID = 1;
-    // packet.destinationCallsign = 'KM6TIG';
-    // packet.destinationSSID = 2;
-    // packet.infoString == 'HELLO Stupid test string';
-    tnc.send(packet.assemble(), () => console.log('Sent:', str));
-}
-
 router.get('/', function (req,res) {
     //res.send('GET handler for the /tnc route');
-    messageString = 'Send test packet';
-    send_string(messageString); 
 
-    tnc.on(
-        "frame",
-        function(frame) {
-            console.log("Here's an array of bytes representing an AX.25 frame: " + frame);
-        }
-    );
-    
-    tnc.on(
-        "error",
-        function(err) {
-            console.log("HURRRRR! I DONE BORKED!" + err);
-        }
-    );
-    res.send({ response: 'Sent:'+ messageString }); 
 });
 
 router.post('/', function (req,res) {
-    res.send('POST handler for the /tnc route');
+    //res.send('POST handler for the /tnc route');
 });
 
 router.get('/sender/:senderCallsign/destination/:destCallsign', function (req,res) {
@@ -95,28 +54,22 @@ router.get('/sender/:senderCallsign/destination/:destCallsign', function (req,re
 });
 
 router.get('/sender/:senderCallsign/destination/:destCallsign/message/:messageText', function (req,res) {
-    res.send(req.params)
+     
 });
 
-router.get('/echotest/kiss', function (req,res, next) {
-    console.log('Set the echo test')
-    next()
-}, function (req, res) {
-    res.send('KISS started')
-});
+
 
 router.get('/func/', function (req,res){
 
-    let fun = req.query.funcId; 
-    switch (fun) {
-        case '1':
-        res.send('First is best')
-        case '2':
-        res.send('Second is ok')
-        default:
-        res.send('DEFAULT')
-    }
+    // let fun = req.query.funcId; 
+    // switch (fun) {
+    //     case '1':
+    //     res.send('First is best');
+    //     case '2':
+    //     res.send('Second is ok');
+    //     default:
+    //     res.send('DEFAULT');
+    // }
 });
 
 module.exports = router;
-
