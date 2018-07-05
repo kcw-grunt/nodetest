62ef93995907600f092ca4af3eeea9312022253d
Kerry <mrkerrywashington@icloud.com>
2018-07-04 22:04:49 -0700

    pass actions to ui

--- routes/tnc.js
+++ routes/tnc.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var ax25 = require('th-d72-ax25');
-const SerialPort = require('serialport');
-//const Readline = SerialPort.parsers.Readline;
-//const port = new SerialPort('/dev/ttyUSB0',9600);
-//const parser = new Readline();
+var SerialPort = require('serialport'); 
 var util = require('util');
+
 var devicePath = '/dev/ttyUSB0'; 
-var currentPacket = new ax25.Packet();
-console.log('Selected port: '+ devicePath +'\n');
 var radiodata ="--NO RESPONSE--";
-var ct = 1;
+var messageContent = "";
+var currentPacket = new ax25.Packet();
+
 var tnc = new ax25.kissTNC(
     {		serialPort : devicePath,
 	  		baudRate : 9600,
@@ -21,72 +19,41 @@ var tnc = new ax25.kissTNC(
     }
 ); 
 
-	var beacon = function(scs,sssid,dcs,dssid,message_tx) {
-		var ssid_s = parseInt(sssid, 10);
-		var ssid_d = parseInt(dssid, 10);
-		scs= ""+scs;
-		dcs = ""+dcs;
-		message_tx = ""+message_tx;
-		var packet = new ax25.Packet(
-			{	'sourceCallsign' : scs,
-				'sourceSSID' : ssid_s,
-				'destinationCallsign' : dcs,
-				'destinationSSID' : ssid_d,
-				'type' : ax25.Defs.U_FRAME_UI,
-				'infoString' : message_tx
-			}
-		);
-		var frame = packet.assemble();
-		tnc.send(frame);
-		console.log("Beacon sent.");
-	}
+console.log('Selected port: '+ devicePath +'\n');
 
-// tnc.pipe(parser);
-// tnc.on('data', function (data) {
-// 	  console.log('data received: ' + data)
-// 	})
-// tnc.write('ROBOT PLEASE RESPOND\n');
-//   console.log("After Robotcall");  
+var beacon = function(scs,sssid,dcs,dssid,message_tx) {
+	var ssid_s = parseInt(sssid, 10);
+	var ssid_d = parseInt(dssid, 10);
+	scs= ""+scs;
+	dcs = ""+dcs;
+	message_tx = ""+message_tx;
+	var packet = new ax25.Packet(
+		{	'sourceCallsign' : scs,
+			'sourceSSID' : ssid_s,
+			'destinationCallsign' : dcs,
+			'destinationSSID' : ssid_d,
+			'type' : ax25.Defs.U_FRAME_UI,
+			'infoString' : message_tx
+		}
+	);
+	var frame = packet.assemble();
+	tnc.send(frame);
+	console.log("Beacon sent.");
+}
 
+function updateLogText(str) {
+	radiodata = str; 
+	//.res.render('tnc', {remote_response:radiodata+"\n"+Date.now()}); //, remote_response:ct 
+}
 
-
-// sp.list(function (err, ports) {
-//     ports.forEach(function(port) {
-//       if (port.comName === '/dev/tty.SLAB_USBtoUART' || port.comName === '/dev/ttyUSB0') {
-//         devicePath = port.comName;
-//       }
-//     });
-// });
-  
-
-
-  
-
-// var beacon = function() {
-// 	var frame = testpacket.assemble();
-// 	tnc.send(frame);
-// 	console.log("Beacon sent");
-// }
-
-// // Switches the port into "flowing mode"
-// serialPort.on('data', function (data) {
-//     console.log('Data:', data);
-// });
-// // Read data that is available but keep the stream from entering //"flowing mode"
-// serialPort.on('readable', function () {
-//     console.log('Data:', port.read());
-// });
-
-function sendTestMessage(scs,sssid,dcs,dssid,message_tx) {
-	
-
-	console.log('inside send test message');
+function sendTestMessage(scs,sssid,dcs,dssid,message_tx) { 
 	console.log(scs + sssid + "\n"+ dcs+ dssid +"\n"+ message_tx+ "\n" );
 	var ssid_s = parseInt(sssid, 10);
 	var ssid_d = parseInt(dssid, 10);
 	scs= ""+scs;
 	dcs = ""+dcs;
 	message_tx = ""+message_tx;
+	messageContent = message_tx;
 
 	console.log('ssds:'+ ssid_s + 'ssdd' + ssid_d);
 	var testpacket = new ax25.Packet(
@@ -98,23 +65,19 @@ function sendTestMessage(scs,sssid,dcs,dssid,message_tx) {
 			'infoString' : message_tx
 		}
 	);
-    currentPacket = testpacket;
-	//console.log(testpacket);
+    currentPacket = testpacket; 
 	var frame = testpacket.assemble();
 	tnc.send(frame);
 	console.log('Test message sent');
 	radiodata = 'Test message sent'; 
 }
-     
-//tnc.startCONV('Hello');
- 
+      
 tnc.on(
 	"opened",
 	function() {
-		console.log("TNC opened on " + tnc.serialPort + " at " + tnc.baudRate);
-		radiodata = "TNC opened on " + tnc.serialPort + " at " + tnc.baudRate;
+		console.log("TNC opened on " + tnc.serialPort + " at " + tnc.baudRate); 
+		updateLogText("TNC opened on " + tnc.serialPort + " at " + tnc.baudRate);
 		//setInterval(beacon, 20000);
-		
 	}
 );
 
@@ -133,14 +96,13 @@ tnc.on(
 		);
 		if(packet.infoString != "")
 			console.log(packet.infoString);
+			radiodata = packet.infoString;
 	}
 );
  
 
 router.get('/', function (req,res) {    
-	res.render('tnc', { title: 'TNC Messaging', remote_response:radiodata+"\n"+Date.now()}); //, remote_response:ct 
-	//res.render('index', { title: 'Onda Terminal Node' });
-
+	res.render('tnc', { title: 'TNC Messaging', message_tx:messageContent, remote_response:radiodata+"\n"+Date.now()});
 });
  
 router.post('/', function (req,res) {
@@ -153,45 +115,20 @@ router.post('/sendmessage', function (req,res) {
 	var sourcecallsign = ""+req.body.srccs
 	var destcallsign = ""+req.body.destcs
 	var destssid = req.body.ssidd
-	var messagetext = ""+req.body.message 
+	var messagetext = ""+req.body.message
 
 	console.log("Message = "+messagetext+"\nSource Callsign = "+sourcecallsign+" "+sourceid+"\nDest Callsign = "+destcallsign+" "+destssid+"\n");
 	
 	if ( sourceid.length > 0 && destssid.length > 0 && sourcecallsign.length > 0 && destcallsign.length > 0 && messagetext.length > 0) {
 		
-		console.log(' Beacon');	
-		
-		res.redirect('/tnc');
-
-		//setInterval(beacon,40000);
-
-		//setInterval(sendTestMessage(sourcecallsign,sourceid,destcallsign,destssid,messagetext),40000);
+		console.log(' Beacon Ping');	
+		updateLogText('Beacoon Ping'); 
 	} else {
-		console.log('stm Failed');
- 	}
-	//res.render('tnc', {remote_response: 'TEST Message'});
-
-	//console.log({source_callsign:sourcecallsign,source_id:sourceid,dest_callsign:destcallsign,dest_ssid:destssid,message_tx:messagetext});
-	// console.log(ms);
-	//res.send('POST for message');
-	//res.redirect('/tnc');
+		console.log('STM Failed');
+		updateLogText('STM Failed');
+	 } 
+	res.render('tnc', { title: 'TNC Messaging', message_tx:messageContent, remote_response:radiodata+"\n"+Date.now()});
   });
 
-// router.get('/func/', function (req,res){
-
-//     // let fun = req.query.funcId; 
-//     // switch (fun) {
-//     //     case '1':
-//     //     res.send('First is best');
-//     //     case '2':
-//     //     res.send('Second is ok');
-//     //     default:
-//     //     res.send('DEFAULT');
-//     // }
-// });
-
 module.exports = router;
-
-function newFunction() {
-	return 'Express';
-}
+ 
