b6c677e78ab70034289b29d1aabef10ce42b8ddf
Kerry <mrkerrywashington@icloud.com>
2018-07-05 19:38:33 -0700

    change app.js to pug

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/terminal', terminalRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 
 
