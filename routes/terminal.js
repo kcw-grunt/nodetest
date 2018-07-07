f8a65ed34ab83de5a9754fd747d166b55df91ed0
Kerry <mrkerrywashington@icloud.com>
2018-07-06 19:36:22 -0700

    clear term

--- routes/terminal.js
+++ routes/terminal.js
@@ -1,20 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req,res) { 
-	res.render('terminal', { title: 'Terminal Page' });
-});
 
-// /* GET Hello World page. */
-// router.get('/terminal', function(req, res) {
-//     //res.render('terminal', { title: 'Hello, Terminal' });
-//     res.send('GET Handler for the /terminal endppint');
-// });
-      
-// router.post('/terminal', function(req, res) {
-//     //res.render('terminal', { title: 'Hello, Terminal' });
-//     res.send('POST Handler for the /terminal endppint');
-// });
 router.get('/sendmessage', function (req,res) { 
     console.log('form info');
 });
