3a2e8a37ad746123303ebd5b969aa9442af94a2b
kcw-grunt <mrkerrywashington@me.com>
2018-07-10 19:07:36 -0700

    add kiss setup

--- routes/tnc.js
+++ routes/tnc.js
@@ -2,36 +2,47 @@ var express = require('express');
 var router = express.Router();
 var ax25 = require('th-d72-ax25');
 var util = require('util');
-var SerialPort = require('serialport');
-var Readline = SerialPort.parsers.Readline;
-var parser = new Readline();
-var devicePath = '/dev/ttyUSB0';
-var osvar = process.platform;
-console.log(osvar);
-if (osvar == 'darwin') {
-	console.log("Using Mac OS");
-    devicePath = '/dev/tty.SLAB_USBtoUART';
-}else{ 
-	devicePath = '/dev/ttyUSB0';
-}
+
+const delay = ms => {
+	return new Promise((resolve) => {
+		setTimeout(resolve,ms);
+	});
+};
+
+const Delimiter = require('@serialport/parser-delimiter')
+const parser = port.pipe(new Delimiter({ delimiter: '\n' }))
+parser.on('data', console.log)
+
+var devicePath = '/dev/KENWOOD_TH-D72A';
+// var osvar = process.platform;
+// console.log(osvar);
+// if (osvar == 'darwin') {
+// 	console.log("Using Mac OS");
+//     devicePath = '/dev/tty.SLAB_USBtoUART';
+// }else{ 
+// 	devicePath = '/dev/ttyUSB0';
+// }
 
 var radiodata ="--NO RESPONSE--";
 var messageContent = ""; 
 
+
 var tnc = new ax25.kissTNC(
-	{		serialPort : devicePath,
-			baudRate : 9600
-	}); 
-tnc.serialHandle.pipe(parser);
-tnc.enterD72KISS();
-//('E ON HBAUD 9600 M ON PASSALL ON KISS ON RESTART');
+	{	serialPort : devicePath,
+		baudRate : 9600
+	}
+);
 
-process.on('unhandledRejection', (reason, promise) => {
-	console.log('PROCESS : Unhandled Rejection at:', reason.stack || reason)
-})
+setupTHD72A();
 
-console.log('Selected port: '+ devicePath +'\n');
- 
+const setupTHD72A = async () => {
+	if (tnc) {
+		tnc.sendRAWPacket('KISS ON');
+		await delay(2000);
+		tnc.sendRAWPacket('RESTART');
+		await delay(4000);
+	}
+}
 function updateLogText(str) {
 	radiodata = str; 
 	//.res.render('tnc', {remote_response:radiodata+"\n"+Date.now()}); //, remote_response:ct 
@@ -116,6 +127,13 @@ tnc.on(
 	}
 );
 
+tnc.on(
+	"data",
+	function(data) {
+		console.log('Data:',data );; 
+	}
+)
+
 // myPort.on('open', showPortOpen);    // called when the serial port opens
 // myPort.on('close', showPortClose);  // called when the serial port closes
 // myPort.on('error', showError);   // called when there's an error with the serial port
@@ -161,5 +179,66 @@ router.post('/sendmessage', function (req,res) {
 //('E ON HBAUD 9600 M ON PASSALL ON KISS ON RESTART');
   });
 
+
+  //////Error Housekeeping
+  process.on('unhandledRejection', (reason, promise) => {
+	console.log('PROCESS : Unhandled Rejection at:', reason.stack || reason)
+  })
+
 module.exports = router;
  
+'use strict';
+var express = require('express');
+var router = express.Router(); 
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+//const Readline = SerialPort.parsers.Readline;  
+var devicePath = '/dev/KENWOOD_TH-D72A';
+var dataLine = [];
+// var osvar = process.platform;
+// console.log(osvar);
+// if (osvar == 'darwin') {
+// 	console.log("Using Mac OS");
+//     devicePath = '/dev/tty.SLAB_USBtoUART';
+// }else{ 
+// 	devicePath = '/dev/ttyUSB2';
+// }
+    
+
+
+ 
+port.write('KISS ON\r\n', function(err) {
+  console.log('KISS ON Turned on');
+});
+
+port.write('RESTART\r\n', function(err) {
+  console.log('Restarted');
+});
+
+port.on('data', function(data) {
+
+  // if(data =='0D 0A') {
+  //   console.log('Date line:'+ dataline)
+  // } else {
+  //   dataLine.push(data);
+  // }
+  console.log('Data:', data);
+});
+
+port.on('open', function() {
+  console.log('Port Opened');
+});
+
+port.on('closed', function() {
+  console.log('Port Closed');
+});
+
+var start = Date.now();
+setInterval(function() {
+    var delta = Date.now() - start; // milliseconds elapsed since start
+     // alternatively just show wall clock time:
+    console.log(new Date().toUTCString());
+    port.write('I\r\n');
+    port.write('Hi Mom!');
+    port.write(new Buffer('Hi Mom!'));
+}, 5000); // update 
